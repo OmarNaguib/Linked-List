971f078dcf928751a2967ecd7445f50a88fa3ac4
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-22 09:41:20 +0300

    Implement prepend, size

--- list.js
+++ list.js
@@ -5,23 +5,38 @@ function ListNode(value = null, next = null) {
 // List functions
 function append(value) {
   // if list is empty
-  if (!this.head) {
-    this.head = ListNode(value);
-  } else {
+  if (!this.head) this.head = ListNode(value);
+  else {
     let currentNode = this.head;
     while (currentNode.next) currentNode = currentNode.next;
     currentNode.next = ListNode(value);
   }
+}
+
+function prepend(value) {
+  this.head = ListNode(value, this.head);
+}
+
+function size() {
+  let currentNode = this.head;
+  let count = 0;
+  while (currentNode) {
+    count += 1;
+    currentNode = currentNode.next;
+  }
+  return count;
 }
 // list object
 function LinkedList(head = null) {
   return {
     head,
     append,
+    prepend,
+    size,
   };
 }
 
 const list = LinkedList();
-list.append(3);
-list.append(3);
-console.log(list);
+list.prepend(1);
+list.prepend(3);
+console.log(list.size());
