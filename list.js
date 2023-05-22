5374e5f6277652cdb58b504f260defb767b4dca0
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-22 09:51:02 +0300

    Implement heaed, tail

--- list.js
+++ list.js
@@ -2,7 +2,7 @@
 function ListNode(value = null, next = null) {
   return { value, next };
 }
-// List functions
+// List methods
 function append(value) {
   // if list is empty
   if (!this.head) this.head = ListNode(value);
@@ -26,6 +26,22 @@ function size() {
   }
   return count;
 }
+
+// no need for head() since it can be accesed as a property
+
+function tail() {
+  let currentNode = this.head;
+  while (currentNode.next) currentNode = currentNode.next;
+  return currentNode;
+}
+function at(index) {}
+function pop() {}
+function contains(value) {}
+function find() {}
+function toString() {}
+function insertAt(value, index) {}
+function removeAt(index) {}
+
 // list object
 function LinkedList(head = null) {
   return {
@@ -33,6 +49,7 @@ function LinkedList(head = null) {
     append,
     prepend,
     size,
+    tail,
   };
 }
 
@@ -40,3 +57,5 @@ const list = LinkedList();
 list.prepend(1);
 list.prepend(3);
 console.log(list.size());
+console.log(list.head);
+console.log(list.tail());
